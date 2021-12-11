3c1ff44134dda4a89a009fda5aa5df9781079ec3
jpsaturnino <jp_saturnino@hotmail.com>
2021-12-11 20:15:52 -0300

    adicionado exceptions, type annotations, coment

--- main.py
+++ main.py
@@ -8,48 +8,67 @@ import requests
 URL = "https://www.gov.br/ans/pt-br/assuntos/prestadores/padrao-para-troca-de-informacao-de-saude-suplementar-2013-tiss"
 
 
-def get_soup(url):
+def get_soup(url: str) -> bs:
+    """
+    Retorna o conteudo da pagina
+    :param url: str
+    :return: bs
+    """
     return bs(requests.get(url).content, "html.parser")
 
 
-def get_nome_arquivo(str):
-    cont = 0
+def get_nome_arquivo(link: str) -> str:
+    """
+    Retorna o nome do arquivo a partir do link
+    :param link: str
+    :return: str
+    :raises: Exception
+    """
     nome_arquivo = ""
-    for i in range(len(str) - 1, 0, -1):
-        if str[i] == "-":
-            cont += 1
+    for i in range(len(link) - 1, 0, -1):
+        if link[i] != "/":
+            nome_arquivo = link[i] + nome_arquivo
         else:
-            if cont == 2:
-                return nome_arquivo
-            nome_arquivo = str[i] + nome_arquivo
+            return nome_arquivo
 
 
-def baixar_tiss(soup):
+def baixar_tiss(soup: bs) -> None:
+    """
+    Faz o download do ultimo arquivo TISS.
+    :param soup: Uma instancia de bs
+    :return: None
+    :raises: Exception
+    """
     links = soup.find_all("a")
     for link in links:
         if "padrao-tiss_componente-organizacional" in link.get("href"):
             with open(
                 get_nome_arquivo(link.get("href")),
                 "wb",
-            ) as f:
-                f.write(requests.get(link.get("href")).content)
+            ) as arquivo_pdf:
+                return arquivo_pdf.write(requests.get(link.get("href")).content)
+    raise Exception("Não foi possivel baixar o arquivo TISS")
 
 
-def buscar_tiss():
+def buscar_tiss() -> bs:
+    """
+    Busca o ultimo arquivo TISS
+    :return: bs
+    :raises: Exception
+    """
     soup = get_soup(URL)
     links = soup.find_all("a")
     for link in links:
         if "padrao-tiss-2013" in link.get("href"):
             return get_soup(link.get("href"))
-    return None
+    raise Exception(
+        "Não foi possivel encontrar o arquivo TISS, verifique o link de acesso!"
+    )
 
 
 def main():
     soup = buscar_tiss()
-    if soup:
-        baixar_tiss(soup)
-    else:
-        print("Scraper desatualizado, verifique os links!")
+    baixar_tiss(soup)
 
 
 __name__ == "__main__" and main()
